6db7dd9f15a30fe799a0d7a9033070d752faf6b5
shangyuhao <834725611@qq.com>
2023-02-23 22:47:51 +0800

    1

--- src/views/layout/layoutView.vue
+++ src/views/layout/layoutView.vue
@@ -1,58 +1,54 @@
 <template>
     <div class="layout">
-      <el-menu
-        default-active="1"
-        class="el-menu-vertical-demo"
-        background-color="#545c64"
-        text-color="#fff"
-        active-text-color="#ffd04b">
+        <el-menu default-active="1" class="el-menu-vertical-demo" background-color="#545c64" text-color="#fff"
+            active-text-color="#ffd04b">
 
 
-      <el-submenu index="1">
-        <template slot="title">
-          <i class="el-icon-location"></i>
-          <span>环境管理</span>
-        </template>
-        <el-menu-item-group>
-          <el-menu-item index="1-1">选项1</el-menu-item>
-          <el-menu-item index="1-2">选项2</el-menu-item>
-        </el-menu-item-group>
-      </el-submenu>
+            <el-submenu index="1">
+                <template slot="title">
+                    <i class="el-icon-location"></i>
+                    <span>环境管理</span>
+                </template>
+                <el-menu-item-group>
+                    <el-menu-item index="1-1">选项1</el-menu-item>
+                    <el-menu-item index="1-2">选项2</el-menu-item>
+                </el-menu-item-group>
+            </el-submenu>
 
-      <el-submenu index="2">
-        <template slot="title">
-          <i class="el-icon-location"></i>
-          <span>能源管理</span>
-        </template>
-        <el-menu-item-group>
-          <el-menu-item index="1-1">选项1</el-menu-item>
-          <el-menu-item index="1-2">选项2</el-menu-item>
-        </el-menu-item-group>
-      </el-submenu>
+            <el-submenu index="2">
+                <template slot="title">
+                    <i class="el-icon-location"></i>
+                    <span>能源管理</span>
+                </template>
+                <el-menu-item-group>
+                    <el-menu-item index="1-1">选项1</el-menu-item>
+                    <el-menu-item index="1-2">选项2</el-menu-item>
+                </el-menu-item-group>
+            </el-submenu>
 
-        <el-menu-item index="3">
-          <i class="el-icon-document"></i>
-          <span slot="title">导航三</span>
-        </el-menu-item>
-        <el-menu-item index="4">
-          <i class="el-icon-setting"></i>
-          <span slot="title">导航四</span>
-        </el-menu-item>
-      </el-menu>
+            <el-menu-item index="3">
+                <i class="el-icon-document"></i>
+                <span slot="title">导航三</span>
+            </el-menu-item>
+            <el-menu-item index="4">
+                <i class="el-icon-setting"></i>
+                <span slot="title">导航四</span>
+            </el-menu-item>
+        </el-menu>
     </div>
-  </template>
+</template>
   
-  <script>
-  
-  
-  export default {
-  
-  }
-  </script>
+<script>
 
-  <style>
-.el-menu-vertical-demo{
-    height: 100vh;
+
+export default {
+
+}
+</script>
+
+<style>
+.el-menu-vertical-demo {
+    min-height: 100vh;
 }
 </style>
   
