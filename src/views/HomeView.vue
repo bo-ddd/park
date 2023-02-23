e554b256193a08d91349df1e8e5b89d6c88ff260
shangyuhao <834725611@qq.com>
2023-02-23 22:01:12 +0800

    init 项目

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,13 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-     <el-button type="success">成功按钮</el-button>
+   
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
