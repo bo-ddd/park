953c9527239f6695c5cf155181086ab80db3409f
shangyuhao <834725611@qq.com>
2023-02-23 22:44:11 +0800

    添加了menu

--- src/views/layout/layoutView.vue
+++ src/views/layout/layoutView.vue
@@ -19,10 +19,17 @@
         </el-menu-item-group>
       </el-submenu>
 
-        <el-menu-item index="2">
-          <i class="el-icon-menu"></i>
-          <span slot="title">导航二</span>
-        </el-menu-item>
+      <el-submenu index="2">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>能源管理</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="1-1">选项1</el-menu-item>
+          <el-menu-item index="1-2">选项2</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+
         <el-menu-item index="3">
           <i class="el-icon-document"></i>
           <span slot="title">导航三</span>
