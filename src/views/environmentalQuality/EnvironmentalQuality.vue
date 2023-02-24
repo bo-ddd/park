e109a771e6384a9744d397f90c1f582b947166e3
zhujiangyan <t2710086269@163.com>
2023-02-24 08:50:11 +0800

    卸载了swiper

--- src/views/environmentalQuality/EnvironmentalQuality.vue
+++ src/views/environmentalQuality/EnvironmentalQuality.vue
@@ -2,16 +2,20 @@
 <template>
     <div class="environmental0-quality">
 
+
+
     </div>
 </template>
 
 <script >
-import 'swiper/dist/js/swiper'
-import 'swiper/dist/css/swiper.css'
-// import Swiper from "swiper"
+
 
 export default {
+ 
 
+mounted(){
+    
+  },
 }
 </script>
 
